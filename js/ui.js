8089824008610c9c8f6753e4fb11f3cd2304654e
Entrequadras <filipebrasilia@gmail.com>
2025-08-10 12:38:28 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -110,7 +110,7 @@
             // Versão (SEMPRE VISÍVEL)
             ctx.fillStyle = '#666';
             this.setPixelFont(ctx, 8);
-            ctx.fillText('Revisão Alpha-13', ctx.canvas.width/2, 115);
+            ctx.fillText('Revisão Alpha-19', ctx.canvas.width/2, 115);
             ctx.textAlign = 'left';
         },
         
