8734b05684cf30125701c322df5c5636e2b5a019
Entrequadras <filipebrasilia@gmail.com>
2025-08-11 21:45:47 -0300

    Update maps.js ares 2

--- js/maps.js
+++ js/maps.js
@@ -237,7 +237,23 @@
                     ]
                 }
             ],
-            trees: [],
+          trees: [
+    // Árvore central
+    {type: 'arvore006', x: 1078, y: 1077},
+    
+    // Linha de árvores na parte inferior (530x1500 até 1130x1500)
+    {type: 'arvorebloco001', x: 530, y: 1420},
+    {type: 'arvore001', x: 680, y: 1450},
+    {type: 'arvore003', x: 780, y: 1440},
+    {type: 'arvore002', x: 850, y: 1455},
+    {type: 'arvore004', x: 950, y: 1435},
+    {type: 'arvore001', x: 1030, y: 1445},
+    {type: 'arvore003', x: 1100, y: 1460},
+    
+    // Duas árvores no topo (1150x50 e 1200x20)
+    {type: 'arvore004', x: 1150, y: 35},
+    {type: 'arvore002', x: 1200, y: 20}
+],
             streetLights: [
                 {type: 'poste000', x: 695, y: 350, rotation: 0, lightRadius: 100, id: 'ks_post1'},
                 {type: 'poste000', x: 355, y: 204, rotation: 0, lightRadius: 100, id: 'ks_post2'},
